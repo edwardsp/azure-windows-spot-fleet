682b5a6ab820895aa50f5701c091b73851ad0413
Paul Edwards <paul.m.edwards@gmail.com>
2021-12-07 09:01:02 +0000

    adding sleep and forcing restart

--- InstallScript.ps1
+++ InstallScript.ps1
@@ -6,7 +6,8 @@ $logfile = 'C:\install\install-progress.txt'
 
 if (-not(Test-Path -Path $logfile -PathType Leaf)) {
     Add-Content -Path 'C:\install\install-progress.txt' -Value "$(Get-TimeStamp) First run of the install script"
-    Restart-Computer
+    Start-Sleep -Seconds 10
+    Restart-Computer -Force
 }
 else {
     Add-Content -Path 'C:\install\install-progress.txt' -Value "$(Get-TimeStamp) Running install script after a reboot"
