3fb0bbc2cd2a17fe0b7102e6b4cd946adead9066
Paul Edwards <paul.m.edwards@gmail.com>
2021-12-07 08:37:40 +0000

    adding a reboot to the install script

--- InstallScript.ps1
+++ InstallScript.ps1
@@ -2,4 +2,12 @@ function Get-TimeStamp {
     return "[{0:MM/dd/yy} {0:HH:mm:ss}]" -f (Get-Date)
 }
 
-Add-Content -Path 'C:\install\install-progress.txt' -Value "$(Get-TimeStamp) Running install script"
+$logfile = 'C:\install\install-progress.txt'
+
+if (-not(Test-Path -Path $logfile -PathType Leaf)) {
+    Add-Content -Path 'C:\install\install-progress.txt' -Value "$(Get-TimeStamp) First run of the install script"
+    Restart-Computer
+}
+else {
+    Add-Content -Path 'C:\install\install-progress.txt' -Value "$(Get-TimeStamp) Running install script after a reboot"
+}
